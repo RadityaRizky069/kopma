749f11b9f7ee490bbdeb63af9a18219aa2fd9b10
RadityaRizky069 <azazellll069@gmail.com>
2026-01-08 00:03:36 +0700

    login register

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function showLogin()
+    {
+        return view('login.login');
+    }
+
+    public function showRegister()
+    {
+        return view('login.register');
+    }
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6|confirmed',
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'role' => 'customer', // DEFAULT
+        ]);
+
+        return redirect()->route('login')
+            ->with('success', 'Registrasi berhasil, silakan login');
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            if (auth()->user()->role === 'admin') {
+                return redirect('/admin');
+            }
+
+            return redirect('/checkout');
+        }
+
+        return back()->with('error', 'Email atau password salah');
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    }
+}
